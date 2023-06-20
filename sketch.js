aa95b700ed551343acbcbd8cae832e4c52686843
seanzshow1 <60274196+seanzshow1@users.noreply.github.com>
2023-06-20 19:51:42 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,10 +19,6 @@ function setup() {
 
   toggleStart = createButton('start/stop').position(10, height + 10).mousePressed(() => {
     if (!seq.isPlaying) {
-      if (!audioInitialized) {
-        initializeAudio(); // Initialize audio only if it hasn't been initialized before
-        audioInitialized = true; // Set the flag to true
-      }
       seq.loop();
       toggleStart.html('Playing');
     } else {
@@ -31,6 +27,10 @@ function setup() {
     }
   });
 
+  // Create a button to initialize audio
+  initializeButton = createButton('Initialize Audio');
+  initializeButton.position(150, height + 10);
+  initializeButton.mousePressed(initializeAudio);
 
   //load sound    file path        
   hh = loadSound('assets/hh.wav');
@@ -83,83 +83,85 @@ function initializeAudio() {
   if (getAudioContext().state === 'suspended') {
     getAudioContext().resume();
   }
+}
 
 
-  function draw() {
 
+function draw() {
+
+}
+function canvasClicked() {
+  let rowClicked = floor(mouseY / height * 3);
+  console.log(rowClicked);
+
+  let columnClicked = floor(mouseX / width * beats)
+  console.log(columnClicked);
+
+  //edit hh 
+  if (rowClicked == 0) {
+    hPat[columnClicked] = invert(hPat[columnClicked]);
+  } else if (rowClicked == 1) {
+    sPat[columnClicked] = invert(sPat[columnClicked]);
+  } else if (rowClicked == 2) {
+    kPat[columnClicked] = invert(kPat[columnClicked]);
   }
-  function canvasClicked() {
-    let rowClicked = floor(mouseY / height * 3);
-    console.log(rowClicked);
 
-    let columnClicked = floor(mouseX / width * beats)
-    console.log(columnClicked);
+  drawMatrix();
+}
 
-    //edit hh 
-    if (rowClicked == 0) {
-      hPat[columnClicked] = invert(hPat[columnClicked]);
-    } else if (rowClicked == 1) {
-      sPat[columnClicked] = invert(sPat[columnClicked]);
-    } else if (rowClicked == 2) {
-      kPat[columnClicked] = invert(kPat[columnClicked]);
+
+function invert(inputBit) {
+  if (inputBit == 0) { return 1 } else { return 0 };
+
+}
+
+function drawMatrix() {
+  background(220);
+  stroke('grey');
+  fill('grey');
+
+  //vertical grid 
+  for (let i = 0; i < beats; i++) {
+    line(i * width / beats, 0, i * width / beats, height);
+  }
+
+  //horizontal grid 
+  for (let i = 0; i < 3; i++) {
+    line(0, i * height / 3, width, i * height / 3)
+  }
+
+  for (let i = 0; i < beats; i++) {
+
+    if (hPat[i] == 1) {
+      ellipse(i * width / beats + width / beats * 0.5, 1 / 6 * height, 10);
     }
 
+    if (sPat[i] == 1) {
+      ellipse(i * width / beats + width / beats * 0.5, 3 / 6 * height, 10);
+    }
+
+    if (kPat[i] == 1) {
+      ellipse(i * width / beats + width / beats * 0.5, 5 / 6 * height, 10);
+    }
+  }
+
+}
+
+
+function sequence(time, beatIndex) {
+  setTimeout(() => {
     drawMatrix();
-  }
+    drawPlayhead(beatIndex);
+  }, time * 1000);
 
+}
 
-  function invert(inputBit) {
-    if (inputBit == 0) { return 1 } else { return 0 };
+function drawPlayhead(beatIndex) {
+  //rectangle
 
-  }
+  // beatIndex = 0; 
+  fill(255, 0, 0, 70);
+  stroke('red');
+  rect(width / beats * beatIndex, 0, width / beats, height);
 
-  function drawMatrix() {
-    background(220);
-    stroke('grey');
-    fill('grey');
-
-    //vertical grid 
-    for (let i = 0; i < beats; i++) {
-      line(i * width / beats, 0, i * width / beats, height);
-    }
-
-    //horizontal grid 
-    for (let i = 0; i < 3; i++) {
-      line(0, i * height / 3, width, i * height / 3)
-    }
-
-    for (let i = 0; i < beats; i++) {
-
-      if (hPat[i] == 1) {
-        ellipse(i * width / beats + width / beats * 0.5, 1 / 6 * height, 10);
-      }
-
-      if (sPat[i] == 1) {
-        ellipse(i * width / beats + width / beats * 0.5, 3 / 6 * height, 10);
-      }
-
-      if (kPat[i] == 1) {
-        ellipse(i * width / beats + width / beats * 0.5, 5 / 6 * height, 10);
-      }
-    }
-
-  }
-
-
-  function sequence(time, beatIndex) {
-    setTimeout(() => {
-      drawMatrix();
-      drawPlayhead(beatIndex);
-    }, time * 1000);
-
-  }
-
-  function drawPlayhead(beatIndex) {
-    //rectangle
-
-    // beatIndex = 0; 
-    fill(255, 0, 0, 70);
-    stroke('red');
-    rect(width / beats * beatIndex, 0, width / beats, height);
-
-  }
+}
